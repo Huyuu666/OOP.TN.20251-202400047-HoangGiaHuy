848002047be5ab67bcff3723fdd4771c068dc212
Hoang Gia Huy <hoanghuy060606@gmail.com>
2025-11-18 18:00:31 +0700

    create class CompactDisc, Track and CompactDiscTest

---
+++ AimsProject/src/com/hust/kstn/models/Track.java
@@ -0,0 +1,27 @@
+package com.hust.kstn.models;
+
+public class Track {
+    private String title;
+    private int length;
+
+    public Track() {}
+
+    public Track(String title, int length) {
+        this.title = title;
+        this.length = length;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + " ("
+                + this.length + ")";
+    }
+}
---
+++ AimsProject/src/com/hust/kstn/test/CompactDiscTest.java
@@ -0,0 +1,31 @@
+package com.hust.kstn.test;
+
+import com.hust.kstn.models.CompactDisc;
+import com.hust.kstn.models.Track;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompactDiscTest {
+    public static void main(String[] args) {
+        Track t1 = new Track("Lac Troi",3);
+        Track t2 = new Track("Bac Phan", 4);
+        Track t3 = new Track("Khong the say", 4);
+
+        List<Track> tracks = new ArrayList<>();
+        tracks.add(t1);
+        tracks.add(t2);
+
+        List<String> artists = new ArrayList<>();
+        artists.add("GH");
+        artists.add("DM");
+
+        List<String> directors = new ArrayList<>();
+        directors.add("GH");
+
+        CompactDisc compactDisc = new CompactDisc("Chill", "Love", 20, artists,
+                directors, tracks);
+
+        System.out.println(compactDisc.toString());
+    }
+}
